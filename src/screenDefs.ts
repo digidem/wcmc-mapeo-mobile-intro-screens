58f8a34d608c808224eb2e8d0cba5c73aa0d65a1
Gregor MacLennan <gmaclennan@digital-democracy.org>
2020-03-03 14:30:03 +0100

    fix: Small adjustments to text

--- src/screenDefs.ts
+++ src/screenDefs.ts
@@ -7,9 +7,11 @@ const wcmcScreens: ScreenType[] = [
     description:
       'Mapeo is for mapping your territory and documenting evidence of deforestation and contamination.',
     image: require('../assets/mapeo-logo-transparent.png'),
-    moreLink: 'Who built Mapeo',
+    moreLink: 'Who built Mapeo?',
     moreText: `
-Mapeo was developed by Digital Democracy in close collaboration with indigenous partners in the Amazon rainforest, including Achuar, Cocama, Harakbut, Kofan, Kichwa, Matsiguenka, Siekopai, Wapichana, Waorani and Yine peoples.
+Mapeo was developed by Digital Democracy in close collaboration with indigenous
+partners in the Amazon rainforest, including Achuar, Cocama, Harakbut, Kofan,
+Kichwa, Matsiguenka, Siekopai, Wapichana, Waorani and Yine peoples.
 
 For more information and case studies about how it is being used see: www.mapeo.world
 `,
@@ -46,17 +48,39 @@ Find out more about ICCAs, and the ICCA consortium see: www.iccaconsortium.org
     moreLink: 'What is WCMC and how will they use my data?',
     moreTitle: 'Use of Information',
     moreText: `
-If you are a member of an indigenous people or local community, you can choose to provide information about your conserved area (ICCA) to the UN Environment World Conservation Monitoring Centre (UNEP-WCMC). UNEP-WCMC manages information on ICCAs in order to demonstrate how important the contributions of indigenous peoples and local communities are for the conservation of our natural world.
+If you are a member of an indigenous people or local community, you can choose
+to provide information about your conserved area (ICCA) to the UN Environment
+World Conservation Monitoring Centre (UNEP-WCMC). UNEP-WCMC manages information
+on ICCAs in order to demonstrate how important the contributions of indigenous
+peoples and local communities are for the conservation of our natural world.
 
-Through the app, you can provide information on the outer boundary and location of your ICCA, alongside information on its basic characteristics. If you would also like to provide more in-depth information, photos, and links to further information, you can contact us to discuss sharing a case study through the ICCA Registry website.
+Through the app, you can provide information on the outer boundary and location
+of your ICCA, alongside information on its basic characteristics. If you would
+also like to provide more in-depth information, photos, and links to further
+information, you can contact us to discuss sharing a case study through the ICCA
+Registry website.
 
-Information is gathered through a series of questions that you can answer within Mapeo. No information will be sent to UNEP-WCMC until you decide to share it. The information you share will be added to a global registry of ICCAs.
+Information is gathered through a series of questions that you can answer within
+Mapeo. No information will be sent to UNEP-WCMC until you decide to share it.
+The information you share will be added to a global registry of ICCAs.
 
-If one of the main objectives of the ICCA is to conserve nature you can also choose to include your information in the World Database on Protected Areas (WDPA). This differs from the ICCA Registry because it stores information on many different conservation measures, including those managed by governments, private organisations and individuals, and indigenous peoples and local communities. This database is widely-used by people around the world to help them make decisions about how land and waters are managed.
+If one of the main objectives of the ICCA is to conserve nature you can also
+choose to include your information in the World Database on Protected Areas
+(WDPA). This differs from the ICCA Registry because it stores information on
+many different conservation measures, including those managed by governments,
+private organisations and individuals, and indigenous peoples and local
+communities. This database is widely-used by people around the world to help
+them make decisions about how land and waters are managed.
 
-By providing information to these databases, communities will help the international community to appreciate the vital connection between indigenous peoples, local communities and biodiversity conservation. It also helps people around the world have an awareness of  ICCAs when they make decisions.
+By providing information to these databases, communities will help the
+international community to appreciate the vital connection between indigenous
+peoples, local communities and biodiversity conservation. It also helps people
+around the world have an awareness of  ICCAs when they make decisions.
 
-However, communities should also consider whether they might face challenges as a result of making their information public. More information on potential benefits, and important issues to consider, is available at: www.iccaregistry.org/en/participate/benefits-and-considerations
+However, communities should also consider whether they might face challenges as
+a result of making their information public. More information on potential
+benefits, and important issues to consider, is available at:
+www.iccaregistry.org/en/participate/benefits-and-considerations
 `,
   },
   {
@@ -95,8 +119,14 @@ However, communities should also consider whether they might face challenges as 
       'Once your community has approved the area you can send it to WCMC. They will review it and then publish it online.',
     image: require('../assets/undraw_software_engineer_lvl5.png'),
     moreLink: 'How will WCMC use your data?',
+    moreTitle: 'Use of your data',
     moreText: `
-Before your information is made public a member of staff from UNEP WCMC will contact you with any questions. Your data will then be submitted for others to review. This is a peer-review process, and ensures that the information published in the databases is accurate. Peer-review is carried out by other indigenous peoples and/or local communities, and sometimes facilitating organisations, in the country where the ICCA is located.
+Before your information is made public a member of staff from UNEP WCMC will
+contact you with any questions. Your data will then be submitted for others to
+review. This is a peer-review process, and ensures that the information
+published in the databases is accurate. Peer-review is carried out by other
+indigenous peoples and/or local communities, and sometimes facilitating
+organisations, in the country where the ICCA is located.
 
 After review, your data will be published on xxxxxx.
 
